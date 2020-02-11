d8da6807e957ff50c627446251ad7b7b0454a87d
Кобозев Александр <a.kobozev@corp.mail.ru>
2020-02-11 16:08:54 +0300

    migrate mopub adapters to androidX

--- mediation/mopub/com/mopub/mobileads/MyTargetAdapterConfiguration.java
+++ mediation/mopub/com/mopub/mobileads/MyTargetAdapterConfiguration.java
@@ -1,8 +1,8 @@
 package com.mopub.mobileads;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.mopub.common.BaseAdapterConfiguration;
 import com.mopub.common.OnNetworkInitializationFinishedListener;
