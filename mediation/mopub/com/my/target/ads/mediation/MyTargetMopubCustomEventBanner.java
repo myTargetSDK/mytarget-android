15b7f761690bdf7a8100999235d9060d8079ed3d
Кобозев Александр <a.kobozev@corp.mail.ru>
2020-03-16 17:52:56 +0300

    fix the way to get dimensions from mopub banners

--- mediation/mopub/com/my/target/ads/mediation/MyTargetMopubCustomEventBanner.java
+++ mediation/mopub/com/my/target/ads/mediation/MyTargetMopubCustomEventBanner.java
@@ -1,25 +1,28 @@
 package com.my.target.ads.mediation;
 
 import android.content.Context;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 
 import com.mopub.MopubCustomParamsUtils;
+import com.mopub.common.DataKeys;
 import com.mopub.common.logging.MoPubLog;
-import com.mopub.common.logging.MoPubLog.AdLogEvent;
 import com.mopub.common.logging.MoPubLog.AdapterLogEvent;
 import com.mopub.mobileads.CustomEventBanner;
 import com.mopub.mobileads.MoPubErrorCode;
 import com.my.target.ads.MyTargetView;
+import com.my.target.ads.MyTargetView.AdSize;
 import com.my.target.common.CustomParams;
 
 import java.util.Map;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import static com.mopub.common.logging.MoPubLog.AdLogEvent.LOAD_FAILED;
 
 public class MyTargetMopubCustomEventBanner extends CustomEventBanner
 {
 	private static final String ADAPTER_NAME = MyTargetMopubCustomEventBanner.class.getSimpleName();
 	private static final String SLOT_ID_KEY = "slotId";
-	private static final String SIZE_KEY = "mytarget_adsize";
 
 	private final MyTargetView.MyTargetViewListener myTargetViewListener = new MyTargetView.MyTargetViewListener()
 	{
@@ -74,24 +77,13 @@ public class MyTargetMopubCustomEventBanner extends CustomEventBanner
 	@Override
 	protected void loadBanner(Context context,
 							  @Nullable CustomEventBannerListener customEventBannerListener,
-							  @Nullable Map<String, Object> stringObjectMap,
-							  @Nullable Map<String, String> stringStringMap)
+							  @Nullable Map<String, Object> localExtras,
+							  @Nullable Map<String, String> serverExtras)
 	{
-		MoPubLog.log(AdapterLogEvent.LOAD_ATTEMPTED, ADAPTER_NAME);
-		if (context == null)
-		{
-			MoPubLog.log(AdapterLogEvent.LOAD_FAILED, ADAPTER_NAME, "", "null context");
-			if (customEventBannerListener != null)
-			{
-				customEventBannerListener.onBannerFailed(MoPubErrorCode.INTERNAL_ERROR);
-			}
-			return;
-		}
-
 		int slotId = -1;
-		if (stringStringMap != null && !stringStringMap.isEmpty())
+		if (serverExtras != null && !serverExtras.isEmpty())
 		{
-			String sslotId = stringStringMap.get(SLOT_ID_KEY);
+			String sslotId = serverExtras.get(SLOT_ID_KEY);
 			if (sslotId != null)
 			{
 				try
@@ -100,7 +92,7 @@ public class MyTargetMopubCustomEventBanner extends CustomEventBanner
 				}
 				catch (NumberFormatException e)
 				{
-					MoPubLog.log(AdLogEvent.LOAD_FAILED, ADAPTER_NAME, "", e.getMessage());
+					MoPubLog.log(LOAD_FAILED, ADAPTER_NAME, "", e.getMessage());
 					e.printStackTrace();
 				}
 			}
@@ -108,37 +100,74 @@ public class MyTargetMopubCustomEventBanner extends CustomEventBanner
 
 		if (slotId < 0)
 		{
-			MoPubLog.log(AdLogEvent.LOAD_FAILED, ADAPTER_NAME, "", "Unable to get slotId from parameter json. Probably Mopub mediation misconfiguration.");
-			if (customEventBannerListener != null)
-			{
-				customEventBannerListener.onBannerFailed(MoPubErrorCode.MISSING_AD_UNIT_ID);
-			}
+			callNoFill(customEventBannerListener);
 			return;
 		}
 
 		bannerListener = customEventBannerListener;
-		int adSize = MyTargetView.AdSize.BANNER_320x50;
-		if (stringObjectMap != null)
+		int adSize;
+		if (localExtras != null)
 		{
-			Object size = stringObjectMap.get(SIZE_KEY);
-			if (size != null)
+			Integer adWidth = (Integer) localExtras.get(DataKeys.AD_WIDTH);
+			Integer adHeight = (Integer) localExtras.get(DataKeys.AD_HEIGHT);
+			adSize = calculateSize(adWidth, adHeight);
+			if (adSize < 0)
 			{
-				adSize = (int) size;
+				callNoFill(customEventBannerListener);
+				return;
 			}
+		}
+		else
+		{
+			callNoFill(customEventBannerListener);
+			return;
 		}
 
 		if (myTargetView == null)
 		{
 			myTargetView = new MyTargetView(context);
 			final CustomParams customParams = myTargetView.getCustomParams();
-			if (customParams != null && stringObjectMap != null)
+			if (customParams != null)
 			{
-				MopubCustomParamsUtils.fillCustomParams(customParams, stringObjectMap);
+				MopubCustomParamsUtils.fillCustomParams(customParams, localExtras);
 			}
 			myTargetView.init(slotId, adSize, false);
 			myTargetView.setListener(myTargetViewListener);
 		}
 		myTargetView.load();
+	}
+
+	private int calculateSize(@Nullable Integer width, @Nullable Integer height)
+	{
+		if (width == null || height == null)
+		{
+			return -1;
+		}
+		if (height <= 50)
+		{
+			return AdSize.BANNER_320x50;
+		}
+		else if (width <= 300 && height <= 250)
+		{
+			return AdSize.BANNER_300x250;
+		}
+		else if (height <= 90)
+		{
+			return AdSize.BANNER_728x90;
+		}
+		return -1;
+	}
+
+	private void callNoFill(@Nullable CustomEventBannerListener customEventBannerListener)
+	{
+		MoPubLog.log(LOAD_FAILED, ADAPTER_NAME,
+					 MoPubErrorCode.NETWORK_NO_FILL.getIntCode(),
+					 MoPubErrorCode.NETWORK_NO_FILL);
+
+		if (customEventBannerListener != null)
+		{
+			customEventBannerListener.onBannerFailed(MoPubErrorCode.NETWORK_NO_FILL);
+		}
 	}
 
 	@Override
